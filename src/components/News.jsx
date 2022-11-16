bfef8f9ad8632a19b3f350ef4dfa40a4c3eaddb5
Ян Юсов <hsifananab@Ans-MacBook-Pro.local>
2022-11-17 01:27:02 +0600

    feat: add news section

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { news } from '../data/data';
 
 const News = () => {
   return (
@@ -8,6 +9,15 @@ const News = () => {
           <h2 className="uppercase text-[38px] leading-[150%] font-bold mb-12">
             Последние новости
           </h2>
+          <div className="flex flex-col gap-[65px]">
+            {news.map((item, index) =>
+              index % 2 === 0 ? (
+                <NewsItem item={item} />
+              ) : (
+                <NewsItem item={item} reverse={true} />
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -15,3 +25,30 @@ const News = () => {
 };
 
 export default News;
+
+const NewsItem = ({ item, reverse }) => {
+  return (
+    <div
+      className="flex gap-[50px] "
+      style={{ flexDirection: reverse ? 'row-reverse' : 'row' }}
+    >
+      <img src={item.image} className="flex-1" alt="" />
+      <div className="flex-1">
+        <h3 className="text-[30px] leading-[150%] capitalize mb-[30px]">
+          {item.title}
+        </h3>
+        <p className="text-[#CACACA] leading-[150%] mb-6">{item.desc}</p>
+        <div className="flex gap-4 mb-12">
+          <img src={item.author.userpic} alt="" />
+          <p className="text-[#F89202]">{item.author.name}</p>
+          <p className="text-[#CACACA]">
+            {new Date(item.author.date).toLocaleDateString()}
+          </p>
+        </div>
+        <button className="border border-[#CACACA] rounded-[5px] px-[30px] py-[17px]">
+          К АЛЬБОМУ
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/data/data.js
+++ src/data/data.js
@@ -18,3 +18,26 @@ export const subscriptions = [
     image: require('../assets/images/premium-bg.jpg'),
   },
 ];
+
+export const news = [
+  {
+    title: 'Music to Be Murdered By',
+    desc: 'Новый альбом Eminem «Music to Be Murdered By» уже доступен в нашем сервисе. На альбоме вас ждёт 20 новых треков.',
+    image: require('../assets/images/music-to-be-murdered-by.jpg'),
+    author: {
+      userpic: require('../assets/icons/avatar.png'),
+      name: 'MUSIC BOX',
+      date: 1579219200000,
+    },
+  },
+  {
+    title: 'Ordinary Man',
+    desc: 'Легенда рока Оззи Озборн спустя 10 лет выпустил новый альбом Ordinary Man. Слушайте в числе первых на Music Box',
+    image: require('../assets/images/ordinary-man.jpg'),
+    author: {
+      userpic: require('../assets/icons/avatar.png'),
+      name: 'MUSIC BOX',
+      date: 1582502400000,
+    },
+  },
+];
